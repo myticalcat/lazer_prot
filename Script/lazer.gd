02ba9924d8f3f073e30e64fa1b2664d3c279a1a8
myticalcat <spongedraw@gmail.com>
2023-11-25 07:17:57 +0700

    fix: can create many lazer all at once

--- Script/lazer.gd
+++ Script/lazer.gd
@@ -2,10 +2,12 @@ extends Node2D
 @export var lazer_rect: ColorRect
 
 
+
 func start_lazering():
 	print('imma firin my lazeeeeer')
 	await flicker(lazer_rect, 0.1, 2)
-	fire(lazer_rect)
+	await fire(lazer_rect)
+	queue_free()
 
 func flicker(obj: ColorRect, initial_interval: float, duration: float, decay_factor: float = 0.9):
 	var end_time = Time.get_ticks_msec() + duration * 1000 
--- Script/lazer_area.gd
+++ Script/lazer_area.gd
@@ -8,17 +8,23 @@ var lazer_prefab = preload("res://Scene/Prefab/lazer.tscn")
 var lazer : Node2D = null
 
 func _process(delta):
-	var is_button_pressed = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-	if is_button_pressed and is_mouse_in:
-		if not is_lazer_crt:
+	check_to_fire(Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT))
+	if not is_instance_valid(lazer):
+		lazer = null
+		
+
+func check_to_fire(mouse_down : bool):
+	if mouse_down and is_mouse_in:
+		if not is_lazer_crt and lazer == null:
 			is_lazer_crt = true
 			start_pos = get_global_mouse_position() - get_parent().position
 			lazer = lazer_prefab.instantiate()
 			lazer.position = start_pos
 			add_sibling(lazer)
 			return
-		update_lazer_scale()
-	elif is_button_pressed and not is_mouse_in:
+		elif is_lazer_crt:
+			update_lazer_scale()
+	elif mouse_down and not is_mouse_in:
 		is_lazer_crt = false
 		if lazer:
 			lazer.queue_free()
@@ -41,6 +47,9 @@ func fire_lazer():
 		lazer.start_lazering()
 	start_pos = Vector2()
 	end_pos = Vector2()
+
+func start_cooldown():
+	print('starting cooldown')
 
 func _on_area_2d_mouse_entered():
 	is_mouse_in = true
